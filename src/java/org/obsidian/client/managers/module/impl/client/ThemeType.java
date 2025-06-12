ff3d9b1c55ec24e1a3054ec8e1b0eceefe909fcb
David7777k <98348298+David7777k@users.noreply.github.com>
2025-06-12 16:11:43 +0300

    feat(gui): modernize clickgui theme

--- src/java/org/obsidian/client/managers/module/impl/client/ThemeType.java
+++ src/java/org/obsidian/client/managers/module/impl/client/ThemeType.java
@@ -11,6 +11,11 @@ public enum ThemeType {
             ColorUtil.getColor(224, 224, 224),
             ColorUtil.getColor(32, 32, 32),
             ColorUtil.getColor(224, 224, 224)
+    ),
+    MODERN(
+            ColorUtil.getColor(79, 148, 205),
+            ColorUtil.getColor(18, 18, 18),
+            ColorUtil.getColor(236, 236, 236)
     );
 
     private final int accentColor;
